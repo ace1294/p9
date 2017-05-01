8a6f9c0683c929659cdb2f538c660b3322149dc4
Nathan Poag <npoag@utexas.edu>
2017-05-01 12:26:29 -0500

    Update Bloom.java

--- src/gammajoin/Bloom.java
+++ src/gammajoin/Bloom.java
@@ -5,14 +5,61 @@
  */
 package gammajoin;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import basicConnector.Connector;
+import basicConnector.WriteEnd;
+import basicConnector.ReadEnd;
+import gammaJoin.HJoin;
+import gammaSupport.BMap;
+import gammaSupport.Relation;
+import gammaSupport.ThreadList;
+import gammaSupport.Tuple;
+
 /**
  *
  * @author Jason
  */
 public class Bloom extends Thread {
-    
-    Bloom () {
-        
+    private WriteEnd outA;
+    private WriteEnd outM;
+    private ReadEnd  in;
+    private BMap mapStore;
+    private int joinKey;
+    Bloom (Connector out1, Connector out2, Connector in, int jKey) {
+    	this.mapStore = BMap.makeBMap();
+    	
+    	this.outA = out1.getWriteEnd();
+    	this.outM = out2.getWriteEnd();
+    	this.outA.setRelation(out1.getRelation());
+    	this.outM.setRelation(Relation.dummy);
+    	this.joinKey = jKey;
+    	this.in = in.getReadEnd();
+    	
+    	ThreadList.add(this);
+    	
     }
+    
+    public void run () {
+        try {
+            
+            Tuple temp = in.getNextTuple();
+            while(true) {
+            	mapStore.setValue(temp.get(joinKey), true);
+            	outA.putNextTuple(temp);
+            	if((temp = in.getNextTuple())  == null)
+            		break;
+            }
+            
+            outM.putNextString(mapStore.getBloomFilter());
+            
+            outA.close();
+            outM.close();
+        }
+        catch (Exception e){
+            Logger.getLogger(HJoin.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }	
     
 }
