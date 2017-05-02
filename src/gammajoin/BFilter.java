e45cb2bd025771b46aa8bef11d774dbf969fe5a6
Nathan Poag <npoag@utexas.edu>
2017-05-01 23:29:18 -0500

    Update BFilter.java

--- src/gammajoin/BFilter.java
+++ src/gammajoin/BFilter.java
@@ -5,18 +5,56 @@
  */
 package gammajoin;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import basicConnector.Connector;
+import basicConnector.ReadEnd;
+import basicConnector.WriteEnd;
+import gammaJoin.HJoin;
+import gammaSupport.BMap;
+import gammaSupport.ThreadList;
+import gammaSupport.Tuple;
+
 /**
  *
  * @author Jason
  */
 public class BFilter extends Thread {
+    private WriteEnd out;
+	private ReadEnd inB;
+    private ReadEnd inM;
+    private int joinKey;
+    private BMap mapStore;
     
-    BFilter() {
+    BFilter(Connector in1, Connector in2, Connector out, int jKey) throws Exception {
+    	this.joinKey = jKey;
+    	this.out = out.getWriteEnd();
+    	this.inB = in1.getReadEnd();
+        this.inM = in2.getReadEnd();
         
+        out.setRelation(in2.getRelation());
+        
+        ThreadList.add(this);
     }
     
     public void run() {
-        
+    	try {
+            mapStore = BMap.makeBMap(inM.getNextString());
+            Tuple t = inB.getNextTuple();
+            while(true){
+                if(mapStore.getValue(t.get(joinKey))){
+                    out.putNextTuple(t);
+                }
+                t = inB.getNextTuple();
+                if(t == null)
+                	break;
+            }
+            out.close();
+        }
+        catch (Exception e) {
+            Logger.getLogger(HJoin.class.getName()).log(Level.SEVERE, null, e);
+        }
     }
     
 }
